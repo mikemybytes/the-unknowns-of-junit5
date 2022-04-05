f76f662f0954bb635b11814c570088b3435fb375
Mike Kowalski <git@mkowalski.net>
2022-04-05 21:47:58 +0200

    Simplify price calculation

--- src/main/java/com/mikemybytes/junit5/pricing/PriceCalculator.java
+++ src/main/java/com/mikemybytes/junit5/pricing/PriceCalculator.java
@@ -5,15 +5,13 @@ class PriceCalculator {
     // could be part of the Price to avoid anemic domain model
 
     Price calculate(Price price, Campaign campaign) {
-        if (price.amount().isLessThan(campaign.discount())) {
-            return Price.minimal();
-        }
         var discountedAmount = price.amount().subtract(campaign.discount());
-        var discountedPrice = new Price(price.name() + " (discounted)", discountedAmount);
-        if (discountedPrice.amount().isLessThan(Price.minimal().amount())) {
+
+        if (discountedAmount.isLessThan(Price.minimal().amount())) {
             return Price.minimal();
         }
-        return discountedPrice;
+
+        return new Price(price.name() + " (discounted)", discountedAmount);
     }
 
 }
